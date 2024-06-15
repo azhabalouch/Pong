3789b80fed0e9b8bf6ef3b772a4508a0adcdee42
azhabalouch <azha.nawaz@belouch.com>
2024-06-15 22:13:21 +0500

    Done lecture 2

--- main.lua
+++ main.lua
@@ -1,18 +1,35 @@
+push = require "push"
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 423
+VIRTUAL_HEIGHT = 240
+
 function love.load()
-	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
 		vsync = true})
 end
 
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
 function love.draw()
+	push:apply('start')
+
 	love.graphics.printf(
 		'Hello World',
 		0,
-		WINDOW_HEIGHT / 2,
-		WINDOW_WIDTH,
+		VIRTUAL_HEIGHT / 2,
+		VIRTUAL_WIDTH,
 		'center')
+
+	push:apply('end')
 end
